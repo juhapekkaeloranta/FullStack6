fbf0b91f1ae97c26979dad723da44224c7a7c648
juhapekkamoilanen <juha-pekka.moilanen@helsinki.fi>
2018-03-08 09:46:21 +0200

    Use thunk, move store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import combinedReducer from './reducers/combinedReducer'
 import { anecdoteInitialization } from './reducers/anecdoteReducer'
 import anecdoteService from './services/anecdotes'
-
-const store = createStore(combinedReducer)
+import store from './store'
 
 anecdoteService.getAll().then(anecdotes =>
   store.dispatch(anecdoteInitialization(anecdotes))
